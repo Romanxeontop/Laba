ce85b1d4e959320044e83d703f1ec94d3bc0d2eb
Haruhi <Haruhi@DESKTOP-5O33OCP>
2023-12-20 22:30:45 +0300

    Add project files.

---
+++ Laba/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Point
+    {
+        int x;
+        int y;
+        char symb;
+
+        public void SetX(int x)
+        {
+            if (x >= 0)
+                this.x = x;
+            else
+                throw new Exception("Значение x не может быть отрицательным");
+        }
+        public void SetY(int y)
+        {
+            if (y >= 0)
+                this.y = y;
+            else
+                throw new Exception("Значение y не может быть отрицательным");
+        }
+        public void SetSym(char symbol)
+        {
+            symb = symbol;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+        }
+    }
+}
---
+++ Laba/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        public static void ClearLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Point p = new Point();
+            int x = 0;
+            int y = 0;
+            char symb = ' ';
+            Console.WriteLine("Если вы хотите прекратить рисовать нажмите пробел");
+            while (true)
+            {
+                Console.Write("Введите символ, который хотите использовать для рисования: ");
+                symb = Convert.ToChar(Console.ReadLine());
+                p.SetSym(symb);
+                ClearLine();
+                Console.Write("Координаты в плоскости x: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                ClearLine();
+                Console.Write("Координаты в плоскости y: ");
+                y = Convert.ToInt32(Console.ReadLine());
+                ClearLine();
+                p.SetX(x);
+                p.SetY(y);
+                p.Draw();
+                Console.SetCursorPosition(0, 1);
+                if (Console.ReadKey().KeyChar.Equals(' ')) break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
